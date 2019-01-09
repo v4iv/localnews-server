a2560b99ed0f4b41c5ca42e95875028aec0384cf
Vaibhav Sharma <vaibhavsharmaatwork@gmail.com>
2019-01-09 23:47:40 +0530

    CORS

--- app.js
+++ app.js
@@ -22,6 +22,14 @@ app.use(cookieParser());
 // Path to Static Files
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
 // Routes
 app.use('/', indexRouter);
 app.use('/api/places', placesRouter);
